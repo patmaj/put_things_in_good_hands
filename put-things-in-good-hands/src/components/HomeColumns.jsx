c87cec1a6d433d1c1330094a3fafd95443abbdf0
patmaj <graffpatrycja@gmail.com>
2022-11-16 13:28:19 +0100

    Done HOmeColumns.jsx

--- put-things-in-good-hands/src/components/HomeColumns.jsx
+++ put-things-in-good-hands/src/components/HomeColumns.jsx
@@ -1,8 +1,22 @@
 const HomeColumns = () => {
     return (
-        <div>
-
-        </div>
+        <section className="columns">
+            <div className='columns__item'>
+                <h1 className='columns__number'>10</h1>
+                <h2 className='columns__title'>ODDANYCH WORKÓW</h2>
+                <p className='columns__content'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab est eveniet illum iste labore nobis placeat quam sequi sint voluptates.</p>
+            </div>
+            <div className='columns__item'>
+                <h1 className='columns__number'>5</h1>
+                <h2 className='columns__title'>WSPARTYCH ORGANIZACJI</h2>
+                <p className='columns__content'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab est eveniet illum iste labore nobis placeat quam sequi sint voluptates.</p>
+            </div>
+            <div className='columns__item'>
+                <h1 className='columns__number'>7</h1>
+                <h2 className='columns__title'>ZORGANIZOWANYCH ZBIÓREK</h2>
+                <p className='columns__content'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab est eveniet illum iste labore nobis placeat quam sequi sint voluptates.</p>
+            </div>
+        </section>
     );
 };
 
