745f3e7d39f486ca62a9200c3dee13f0e051ea6c
patmaj <graffpatrycja@gmail.com>
2022-11-21 11:53:55 +0100

    Created Pagination.jsx Posts.jsx data.js , Updated carousel

--- put-things-in-good-hands/src/components/Foundation/HomeFoundation.jsx
+++ put-things-in-good-hands/src/components/Foundation/HomeFoundation.jsx
@@ -1,14 +1,16 @@
 import Decoration from "../assets/Decoration.svg";
-import React, {useState} from "react";
-// import Posts from "./Posts";
-// import Pagination from "./Pagination";
+import React, {useEffect, useState} from "react";
+import Posts from "./Posts";
+import Pagination from "./Pagination";
 import FoundationButton from "./FoundationButton";
 
 const HomeFoundation = () => {
-    const [selected, setSelected] = useState("foundation")
-    // const [data, setData] = useState([])
-    // const [currentPage, setCurrentPage] = useState(1)
+    const [selected, setSelected] = useState("foundation");
+    const [data, setData] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [postsPerPage] = useState(3);
 
+//list for buttons
     const list = [
         {
           id: 'foundation',
@@ -22,7 +24,28 @@ const HomeFoundation = () => {
             id: 'local',
             title: 'Lokalnym zbiórkom',
         },
-        ];
+    ];
+    //change data on button click
+    useEffect(() => {
+        switch (selected) {
+            case 'foundations':
+                setData(foundations);
+                break;
+            case 'locals':
+                setData(locals);
+                break;
+            case 'organizations':
+                setData(organizations);
+                break;
+            default:
+                setData(foundations);
+        }
+    }, [selected]);
+
+    //obecna strona
+    const indexOfLastPost = currentPage * postsPerPage;
+    const indexOfFirst = indexOfLastPost - postsPerPage;
+    const currentPosts = data/s;oce(indexOfFirstPost, indexOfLastPost);
 
     return (
         <section className="foundation">
@@ -44,13 +67,13 @@ const HomeFoundation = () => {
                     )
                 })}
             </div>
-            {/*<Posts data={} />*/}
-            {/*<div className="foundation__pagination">*/}
-            {/*    <Pagination></Pagination>*/}
-            {/*</div>*/}
+            <Posts data={currentPosts} />
+            <div className="foundation__pagination">
+                <Pagination postsPerPage={postsPerPage} totalPosts={data.length} paginate={paginate}/>
+            </div>
 
         </section>
     );
-}
+};
 
 export default HomeFoundation;
