fdad996539b25444448c2f4895146a2a81d09d49
patmaj <graffpatrycja@gmail.com>
2022-11-23 13:20:53 +0100

    Created structure of form

--- put-things-in-good-hands/src/components/HomeContact.jsx
+++ put-things-in-good-hands/src/components/HomeContact.jsx
@@ -7,11 +7,29 @@ const HomeContact = () => {
             <div className='contact__container'>
                 <h1 className="contact__title">Skontaktuj się z nami</h1>
                 <img src={Decoration} className="contact__decor" alt="" />
-
+                <form className="contact__form">
+                    <div className="contact__form-container">
+                        <div className="contact__userinfo">
+                            <div className="contact__userinfo-item">
+                                <label className="contact__label">Wpisz imię</label>
+                                <input className='contact__iput' type="text" name='name' />
+                            </div>
+                            <div className="contact__userinfo-item">
+                                <label className="contact__label">Wpisz swój email</label>
+                                <input className='contact__iput'  name='email' />
+                            </div>
+                        </div>
+                        <div className="contact__msg">
+                            <label className="contact__label">Wpisz swoją wiadomość</label>
+                            <textarea name="message" />
+                        </div>
+                    </div>
+                    <button className="button--small" type='submit'>Wyślij</button>
+                </form>
             </div>
 
         </div>
     );
-}
+};
 
 export default HomeContact;
