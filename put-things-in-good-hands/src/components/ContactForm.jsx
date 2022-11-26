a9a18f8b03b60b1c08a0071e0f504470b9b8b8f9
patmaj <graffpatrycja@gmail.com>
2022-11-26 14:24:41 +0100

    Try to solved problem with hook useEffects in ContactForm.jsx

--- put-things-in-good-hands/src/components/ContactForm.jsx
+++ put-things-in-good-hands/src/components/ContactForm.jsx
@@ -1,29 +1,84 @@
+import React, {useState,useEffect} from "react";
 import Decoration from "../assets/Decoration.svg";
 
 const ContactForm = () => {
+    const initialValues = {name: "", email: "", message: ""};
+    const [formValues, setFormValues] = useState(initialValues);
+    const [formErrors, setFormErrors] = useState({});
+    const [isSubmit, setIsSubmit] = useState(false);
+
+
+    const handleChange = (e) => {
+        const {name, value} = e.target
+        setFormValues({...formValues, [name]: value});
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setFormErrors(validate(formValues));
+        setIsSubmit(true);
+    };
+
+    useEffect(()=> {
+        if (Object.keys(formErrors).length === 0 && isSubmit) {
+            fetch('https://fer-api.coderslab.pl/v1/portfolio/contact', {
+                method: 'POST',
+                headers: {
+                    // 'Accept': 'application/json',
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(formValues)
+            })
+                .then(res => res.json())
+                .then(data => console.log(data))
+                .catch(err => console.log(err));
+        }
+    }, [isSubmit])
+
+
+    const validate = (values) => {
+        const errors = {};
+        const re = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+        if (!values.name || values.split('').length > 1) {
+            errors.name = "Podane imię jest nieprawidłowe!"
+        }
+        if(!values.email || !re.test(values.email)) {
+            errors.email = "Podany email jest nieprawidłowy!"
+        }
+        if(values.message.length < 120) {
+            errors.message = "Wiadomość musi mieć conajmniej 120 znaków!";
+        }
+        return errors;
+    };
+
     return(
 
     <div className='contact__container'>
         <h1 className="contact__title">Skontaktuj się z nami</h1>
         <img src={Decoration} className="contact__decor" alt="" />
-        <form className="contact__form">
+        {Object.keys(formErrors).length === 0 && isSubmit &&
+            <spam className="contact__msg-success">Wiadomość została wysłana! <br/> Wkrótce się skontaktujemy</spam>}
+        <form className="contact__form" onSubmit={handleSubmit}>
             <div className="contact__form-container">
                 <div className="contact__userinfo">
                     <div className="contact__userinfo-item">
                         <label className="contact__label">Wpisz imię</label>
-                        <input className='contact__input' type="text" name='name' placeholder='Krzysztof' />
-                        <span id='name-error'>Podane imię jest nieprawidłowe!</span>
+                        <input className={formErrors.name ? 'contact__input--active' : 'contact__input'}
+                               type="text" name='name' value={formValues.name} onChange={handleChange} />
+                        <span className='contact__error'>{formErrors.name}</span>
                     </div>
                     <div className="contact__userinfo-item">
                         <label className="contact__label">Wpisz swój email</label>
-                        <input className='contact__input'  name='email' placeholder='abc@xyz.pl' />
-                        <span id='email-error'>Podany mail jest nieprawidłowy!</span>
+                        <input className={formErrors.email ? "contact__input--active" : "contact__input"}
+                               name='email' value={formValues.email} onChange={handleChange}/>
+                        <span className='contact__error'>{formErrors.email}</span>
                     </div>
                 </div>
                 <div className="contact__msg">
                     <label className="contact__label">Wpisz swoją wiadomość</label>
-                    <textarea  className="contact__textarea" name="message" placeholder='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'/>
-                    <span id='msg-error'> Wiadomość musi mieć conajmniej 120 znaków!</span>
+                    <textarea  className={formErrors.message ?"contact__textarea--active" : "contact__textarea"}
+                               name="message" value={formValues.message} onChange={handleChange}/>
+                    <span className='contact__error'>{formErrors.message}</span>
                 </div>
             </div>
             <button className="button--small" type='submit'>Wyślij</button>
