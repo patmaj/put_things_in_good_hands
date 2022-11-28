b53b0e7c96734d517a7f15ab349a049217e6c221
patmaj <graffpatrycja@gmail.com>
2022-11-28 13:38:12 +0100

    solved problem with No Routes matched location '//logowanie'

--- put-things-in-good-hands/src/App.js
+++ put-things-in-good-hands/src/App.js
@@ -5,7 +5,7 @@ const App = () => (
     <BrowserRouter>
         <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
+            <Route path="*" element={<Login />} />
             <Route path="/register" element={<Register />} />
         </Routes>
     </BrowserRouter>
