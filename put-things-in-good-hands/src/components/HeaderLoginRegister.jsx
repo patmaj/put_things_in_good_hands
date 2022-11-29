086c183f54d55696584ce0088784e5c744d7f36b
patmaj <graffpatrycja@gmail.com>
2022-11-29 16:55:24 +0100

    Finished style of  _headerLoginRegister.scss

--- put-things-in-good-hands/src/components/HeaderLoginRegister.jsx
+++ put-things-in-good-hands/src/components/HeaderLoginRegister.jsx
@@ -3,10 +3,12 @@ import React from "react";
 
 const HeaderLoginRegister = () => {
      return (
+        <div className="header__right-column">
          <div className="header__login">
              <RouterLink className="header__link" to={'/logowanie'}>Zaloguj</RouterLink>
              <RouterLink className="header__link header__link--register" to={'/rejestracja'}>Załóż konto</RouterLink>
          </div>
+        </div>
      )
 }
 
