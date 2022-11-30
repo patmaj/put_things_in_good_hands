786b021c6b18759f7d37d71c1f2e10b4881be9fc
patmaj <graffpatrycja@gmail.com>
2022-11-30 14:44:11 +0100

    finished validation

--- put-things-in-good-hands/src/components/Register.jsx
+++ put-things-in-good-hands/src/components/Register.jsx
@@ -2,9 +2,59 @@ import HeaderLoginRegister from "./HeaderLoginRegister";
 import HeaderNavbar from "./HeaderNavbar";
 import Decoration from "../assets/Decoration.svg";
 import {Link as RouterLink} from "react-router-dom";
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 const Register = () => {
+
+    const [formValues, setFormValues] = useState({email: "", password:"", password2:""});
+    const [errors, setErrors] = useState({email: ""});
+    const [isSubmit, setIsSubmit] = useState(false);
+
+    const handleChange = (e) => {
+        const {name, value} = e.target;
+        setFormValues({...formValues, [name]: value});
+    }
+
+    const validate = (values) => {
+        const errors = {};
+        const {email, password, password2} = values;
+        const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+        if (!email) {
+            errors.email = "Brak email";
+        } else if (!regex.test(email)) {
+            errors.email = "Podany email jest nieprawidłowy";
+        }
+        if (!password) {
+            errors.password = "Brak hasła"
+        } else if (password.length < 6) {
+            errors.password = "Podane hasło jest za krótkie"
+        }
+        if (!password2) {
+            errors.password2 = "Brak hasła"
+        } else if (password.length < 6) {
+            errors.password = "Podane hasło jest za krótkie"
+        }
+        if (password2.length < 6) {
+            errors.password2 = "Podane hasło jest za krótkie"
+        } else if (password !== password2) {
+            errors.password = "Hasła muszą być identyczne"
+        }
+        if (password2 !== password) {
+            errors.password2 = "Hasła muszą być identyczne"
+        }
+        return errors;
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setErrors(validate(formValues));
+    }
+
+    useEffect(() => {
+        Object.keys(errors).length === 0 ? setIsSubmit(true) : setIsSubmit(false);
+        console.log(isSubmit)
+    }, [errors, isSubmit])
+
     return (
         <div>
             <section className="register">
